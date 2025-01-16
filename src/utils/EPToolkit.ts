18771651419cf0742e787f9f8c3b3346628cd5e0
maicomstoppa <maicom.stoppa@gmail.com>
2025-01-16 09:38:47 -0300

    Update EPToolkit.ts
    Revert CD fix

--- src/utils/EPToolkit.ts
+++ src/utils/EPToolkit.ts
@@ -23,8 +23,8 @@ const cm_start_bytes = Buffer.from([27, 97, 1, 27, 33, 16, 28, 33, 8]);
 const cm_end_bytes = Buffer.from([27, 33, 0, 28, 33, 0]);
 const cb_start_bytes = Buffer.from([27, 97, 1, 27, 33, 48, 28, 33, 12]);
 const cb_end_bytes = Buffer.from([27, 33, 0, 28, 33, 0]);
-const cd_start_bytes = Buffer.from([27, 97, 1, 27, 33, 32]);//Buffer.from([27, 97, 1, 27, 33, 32, 28, 33, 4]);
-const cd_end_bytes = Buffer.from([27, 33, 0, 28, 33, 0]);//Buffer.from([27, 33, 0, 28, 33, 0]);
+const cd_start_bytes = Buffer.from([27, 97, 1, 27, 33, 32, 28, 33, 4]);
+const cd_end_bytes = Buffer.from([27, 33, 0, 28, 33, 0]);
 const d_start_bytes = Buffer.from([27, 33, 32, 28, 33, 4]);
 const d_end_bytes = Buffer.from([27, 33, 0, 28, 33, 0]);
 
